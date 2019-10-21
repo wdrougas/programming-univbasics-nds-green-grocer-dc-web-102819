919025ce8e9c9d78c345757fe1e4355fa3ef674e
Will Drougas <wdrougas@gmail.com>
2019-10-21 13:51:14 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,12 @@
+binding 'pry'
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  i = 0
+  while i < collection.length do
+    return collection[i] if name == collection[i][:item]
+    i += 1
+  end
+nil
 end
 
 def consolidate_cart(cart)
